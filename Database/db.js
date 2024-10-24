021e156a3f2b25e1663249c08b4c58a840619faf
mna <mn45994445@gmail.com>
2024-10-24 23:10:41 +0500

    first

--- Database/db.js
+++ Database/db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Database = () => {
   try {
     mongoose.connect(`mongodb://127.0.0.1:27017/jamia`, {
-      // useUnifiedTopology: true,
-      // useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
     });
     console.log("Database Connected");
   } catch (error) {
